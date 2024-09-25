1e602d6a1ab256a332e6323f1b133cb78a7ef56e
AyeshaKhan17 <114716137+AyeshaKhan17@users.noreply.github.com>
2024-09-25 14:23:18 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',  // Include all HTML files in the src folder and subfolders
+    './src/**/*.js',    // Include all JavaScript files in the src folder and subfolders (if you are using JS)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
